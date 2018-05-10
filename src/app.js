bc49d72a7fa3a8863f357a8b43b0e374a145108d
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-05-09 22:19:03 -0300

    Disable html5 mode

--- src/app.js
+++ src/app.js
@@ -14,11 +14,6 @@ angular.module('app', [
   Components.name,
   Services.name
 ])
-  .config(($locationProvider) => {
-    'ngInject';
-    // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
-    // #how-to-configure-your-server-to-work-with-html5mode
-    $locationProvider.html5Mode(true).hashPrefix('!');
-  })
+  .config(() => {})
 
   .component('app', AppComponent);
